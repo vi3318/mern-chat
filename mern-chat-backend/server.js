acbc4b754c964c20ab19a5416b3d50d75de78763
Vihaan Dharia <vihaandharia04@gmail.com>
2024-03-31 15:20:26 +0530

    third-commit

--- mern-chat-backend/server.js
+++ mern-chat-backend/server.js
@@ -8,7 +8,13 @@ const cors = require('cors');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ['https://deploy-mern-lhwq.vercel.app'],
+    methods: ['POST', 'GET'],
+    credentials: true
+  }
+));
 
 app.use('/users', userRoutes)
 require('./connection')
