0221d555b889fa313e5df31ee900f2d352df7d8d
Vihaan Dharia <vihaandharia04@gmail.com>
2024-03-31 15:49:27 +0530

    final-commit

--- mern-chat-backend/server.js
+++ mern-chat-backend/server.js
@@ -10,11 +10,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors(
   {
-<<<<<<< HEAD
     origin: ['https://mern-chat-frontend-delta.vercel.app'],
-=======
-    origin: ['mern-chat-frontend-delta.vercel.app'],
->>>>>>> 5babe8f33d9925df3dd5124d5c2862f644429229
     methods: ['POST', 'GET'],
     credentials: true
   }
@@ -32,7 +28,71 @@ const io = require('socket.io')(server, {
   }
 });
 
-// Rest of your socket.io and server logic remains the same
+async function getLastMessagesFromRoom(room){
+  let roomMessages = await Message.aggregate([
+    {$match: {to: room}},
+    {$group: {_id: '$date', messagesByDate: {$push: '$$ROOT'}}}
+  ])
+  return roomMessages;
+}
+
+function sortRoomMessagesByDate(messages){
+  return messages.sort(function(a, b){
+    let date1 = a._id.split('/');
+    let date2 = b._id.split('/');
+
+    date1 = date1[2] + date1[0] + date1[1]
+    date2 =  date2[2] + date2[0] + date2[1];
+
+    return date1 < date2 ? -1 : 1
+  })
+}
+
+io.on('connection', (socket)=> {
+
+  socket.on('new-user', async ()=> {
+    const members = await User.find();
+    io.emit('new-user', members)
+  })
+
+  socket.on('join-room', async(newRoom, previousRoom)=> {
+    socket.join(newRoom);
+    socket.leave(previousRoom);
+    let roomMessages = await getLastMessagesFromRoom(newRoom);
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    socket.emit('room-messages', roomMessages)
+  })
+
+  socket.on('message-room', async(room, content, sender, time, date) => {
+    const newMessage = await Message.create({content, from: sender, time, date, to: room});
+    let roomMessages = await getLastMessagesFromRoom(room);
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    // sending message to room
+    io.to(room).emit('room-messages', roomMessages);
+    socket.broadcast.emit('notifications', room)
+  })
+
+  app.delete('/logout', async(req, res)=> {
+    try {
+      const {_id, newMessages} = req.body;
+      const user = await User.findById(_id);
+      user.status = "offline";
+      user.newMessages = newMessages;
+      await user.save();
+      const members = await User.find();
+      socket.broadcast.emit('new-user', members);
+      res.status(200).send();
+    } catch (e) {
+      console.log(e);
+      res.status(400).send()
+    }
+  })
+
+});
+
+app.get('/rooms', (req, res)=> {
+  res.json(rooms)
+})
 
 server.listen(PORT, ()=> {
   console.log('listening to port', PORT);
