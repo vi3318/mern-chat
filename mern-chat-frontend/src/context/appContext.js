c4ba6de667a80cd9873a390b18de8da6aeba92b4
Vihaan Dharia <vihaandharia04@gmail.com>
2024-03-31 17:38:09 +0530

    final1

--- mern-chat-frontend/src/context/appContext.js
+++ mern-chat-frontend/src/context/appContext.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "mern-chat-omega.vercel.app";
+const SOCKET_URL = "https://mern-chat-omega.vercel.app";
 export const socket = io(SOCKET_URL);
 // app context
 export const AppContext = React.createContext();
