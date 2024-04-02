e7d777e6c721a3d20d33f78c5f12ad5bb62d89b8
Vihaan Dharia <vihaandharia04@gmail.com>
2024-04-02 16:54:21 +0530

    cors

--- mern-chat-backend/server.js
+++ mern-chat-backend/server.js
@@ -8,11 +8,7 @@ const cors = require('cors');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-  origin: ['https://mern-chat-frontend-delta.vercel.app'],
-  methods: ['POST', 'GET'],
-  credentials: true
-}));
+app.use(cors({ origin: "*" }))
 
 app.use('/users', userRoutes);
 require('./connection');
